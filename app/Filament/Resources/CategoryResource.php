1c8b24559ca21f40812c2b8fd67138c5ad3c0bc8
Perdana Wahyu Jatmiko <118588435+perdanawjatmiko@users.noreply.github.com>
2024-09-28 02:04:51 +0700

    fix:update grouping

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -3,23 +3,17 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\CategoryResource\Pages;
-use App\Filament\Resources\CategoryResource\RelationManagers;
 use App\Models\Category;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
-use Filament\Tables\Grouping\Group;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Facades\Auth;
 
 class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
-
     protected static ?string $navigationIcon = 'heroicon-o-folder';
 
     public static function form(Form $form): Form
@@ -31,8 +25,9 @@ class CategoryResource extends Resource
                     ->required()
                     ->maxLength(255),
                 Forms\Components\Select::make('parent_id')
-                    ->options(Category::where('parent_id', null)->pluck('name', 'id'))
-                    ->label('Parent'),
+                    ->label('Parent')
+                    ->options(Category::whereNull('parent_id')->pluck('name', 'id'))
+                    ->searchable(),
                 Forms\Components\Textarea::make('description')
                     ->columnSpanFull(),
             ]);
@@ -44,57 +39,30 @@ class CategoryResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->label('Category Name')
-                    ->description(fn (Category $record): string => $record->description ?? '')
+                    ->description(fn ($record) => $record->description)
                     ->searchable()
-                    ->weight(function ($record) {
-                        return is_null($record->parent_id) ? 'bold' : 'normal';
-                    })
-                    ->color(function ($record) {
-                        return $record->parent_id ? '' : 'primary';
-                    })
-                    ->size(function ($record){
-                        return $record->parent_id? 'sm' : 'large';
-                    })
-                    ->disabledClick(),
-                Tables\Columns\TextColumn::make('user.name')
-                    ->label('User Name')
-                    ->searchable()
-                    ->disabledClick(),
+                    ->weight(fn ($record) => is_null($record->parent_id) ? 'bold' : 'normal')
+                    ->color(fn ($record) => is_null($record->parent_id) ? 'primary' : ''),
                 Tables\Columns\TextColumn::make('parent.name')
                     ->badge()
-                    ->label('Parent')
-                    ->toggleable(isToggledHiddenByDefault: true)
-                    ->sortable()
-                    ->disabledClick(),
-                Tables\Columns\TextColumn::make('deleted_at')
-                    ->dateTime()
-                    ->sortable()
+                    ->label('Parent Category')
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
+            ->modifyQueryUsing(function (Builder $query) {
+                return $query
+                    ->with('children')
+                    ->orderByRaw('IF(parent_id IS NULL, id, parent_id), id');
+            })
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
             ])
-            ->query(function (Category $category) {
-                $parents = Category::whereNull('parent_id')->get();
-                $orderedCategories = [];
-                
-                foreach ($parents as $parent) {
-                    $orderedCategories[] = $parent;
-                    $children = Category::where('parent_id', $parent->id)->get();
-                    foreach ($children as $child) {
-                        $orderedCategories[] = $child;
-                    }
-                }
-                return $category->whereIn('id', collect($orderedCategories)->pluck('id'));
-            })
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
@@ -102,11 +70,9 @@ class CategoryResource extends Resource
                 Tables\Actions\RestoreAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
-                ]),
+                Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\ForceDeleteBulkAction::make(),
+                Tables\Actions\RestoreBulkAction::make(),
             ]);
     }
 
@@ -115,13 +81,5 @@ class CategoryResource extends Resource
         return [
             'index' => Pages\ManageCategories::route('/'),
         ];
-    }
-
-    public static function getEloquentQuery(): Builder
-    {
-        return parent::getEloquentQuery()
-            ->withoutGlobalScopes([
-                SoftDeletingScope::class,
-            ]);
     }
 }
