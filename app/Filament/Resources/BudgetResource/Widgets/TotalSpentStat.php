f65506c660976b1c504bbe8fe62b940f6ea48f55
Perdana Wahyu Jatmiko <118588435+perdanawjatmiko@users.noreply.github.com>
2024-09-30 22:38:03 +0700

    add widget total budgets

--- app/Filament/Resources/BudgetResource/Widgets/TotalSpentStat.php
+++ app/Filament/Resources/BudgetResource/Widgets/TotalSpentStat.php
@@ -3,31 +3,25 @@
 namespace App\Filament\Resources\BudgetResource\Widgets;
 
 use App\Filament\Resources\BudgetResource\Pages\ManageBudgets;
+use Filament\Widgets\Concerns\InteractsWithPageTable;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use App\Models\Budget;
-use Filament\Widgets\Concerns\InteractsWithPageTable;
-use Illuminate\Support\Facades\Auth;
 
-class TotalBudgetStat extends BaseWidget
+class TotalSpentStat extends BaseWidget
 {
-    use InteractsWithPageTable;
 
+    use InteractsWithPageTable;
     protected function getTablePage(): string
     {
         return ManageBudgets::class;
     }
+
     protected function getStats(): array
     {
-        $getBudget = Budget::where('user_id', Auth::user()->id)->get();
-        $totalBudget = $this->getPageTableQuery()->sum('amount_target');
         $totalSpent = $this->getPageTableQuery()->sum('amount_now');
-        $percent = round(($totalSpent / $totalBudget) * 100, 2);
-
+        
         return [
-            Stat::make('Total Budgets', 'Rp. '. number_format($totalBudget, 0, '.', '.')),
             Stat::make('Total Spent', 'Rp. '. number_format($totalSpent, 0, '.', '.')),
-            Stat::make('Total Completion', $percent . '%')->color('success'),
         ];
     }
 }
